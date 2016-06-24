f8907cbcc8364ebd3447c657458649d4231205ab
Tsu-Han <bkyo0829@hotmail.com>
2016-06-24 14:47:02 +0800

    variables

--- GithubTest/ViewController.swift
+++ GithubTest/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let x="ddd"
     }
 
     override func didReceiveMemoryWarning() {
