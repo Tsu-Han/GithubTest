f7a4651f46aa44d217b4ee0a4ea0f82abc52fd6b
Tsu-Han <bkyo0829@hotmail.com>
2016-06-24 14:49:19 +0800

    Revert "Revert "variable2""
    This reverts commit 9bb8fcb8733eaf9a9475518bfb382bb17e5efc33.

--- GithubTest/ViewController.swift
+++ GithubTest/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         let x="ddd"
+        let y=x+"y"
     }
 
     override func didReceiveMemoryWarning() {
